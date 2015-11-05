14fa5fadd715993ddb38abeb01bdcbc1471c5309
Ho Pang Au Yeung <hp.auyeung91@gmail.com>
2015-11-04 18:36:54 -0800

    Changed Footer and migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104194318) do
+ActiveRecord::Schema.define(version: 20151105020745) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151104194318) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "colour"
   end
 
 end
