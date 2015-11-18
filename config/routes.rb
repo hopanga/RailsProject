90e8732e75899537ef8c91f6c350ee8c5faa4d2f
Ho Pang Au Yeung <hp.auyeung91@gmail.com>
2015-11-18 13:05:13 -0800

    Added routes for paths, all paths added to links, root page changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
+  get 'static_pages/about', :as => 'about_page'
+  get 'static_pages/contact', :as => 'contact_page'
+  get 'static_pages/index', :as => 'index_page'
+  get 'static_pages/item', :as => 'item_page'
+  get 'static_pages/request', :as => 'request_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
