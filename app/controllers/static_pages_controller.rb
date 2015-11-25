d1c9dd4b08257e45957742b80355e098a002cbaa
Ho Pang Au Yeung <hp.auyeung91@gmail.com>
2015-11-25 15:30:21 -0800

    Edited email path for pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
   @email = params[:email]
   @message = params[:message]
   ActionMailer::Base.mail(:from => @email,
-        :to => 'your-email@example.com',
+        :to => 'hp.auyeung91@gmail.com',
         :subject => "A new contact form message from #{@name}",
         :body => @message).deliver
   end
