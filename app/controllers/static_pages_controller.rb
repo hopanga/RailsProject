5dc60606f1c8e97fcd8e12bc77820a78b5628373
Ho Pang Au Yeung <hp.auyeung91@gmail.com>
2015-11-25 15:40:00 -0800

    Edited page controller for UserMailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,9 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-        :to => 'hp.auyeung91@gmail.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver
+	@name = params[:name]
+	email = params[:email]
+	@message = params[:message]
+	UserMailer.contact_form(@email, @name, @message).deliver
   end
 end
